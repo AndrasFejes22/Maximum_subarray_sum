cc384d8fd9a235c7111305be44c415aa17dd7098
AndrasFejes22 <afejes22@gmail.com>
2023-05-23 23:13:53 +0200

    code optimized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,46 +9,47 @@ public class Main {
         list.add(9L);
         list.add(9L);
         list.add(5L);
-        System.out.println("ArrayList : " + list.toString());
+        //System.out.println("ArrayList : " + list.toString());
 
         int sum = list.stream().mapToInt(Long::intValue).sum();
         int max = list.stream().mapToInt(Long::intValue).max().orElseThrow();
 
-        System.out.println("sum : " + sum);
-        System.out.println("max : " + max);
+        System.out.println("max(list): "+sum(list));
+
+        //System.out.println("sum : " + sum);
+        //System.out.println("max : " + max);
         //System.out.println("subLists : " + subLists(list, 7));
         List<Long> list2 = new ArrayList<Long>();
         list2.add(1L);
         list2.add(5L);
         list2.add(9L);
-        System.out.println("subLists : " + subLists(list2, 5));
+        System.out.println("subLists : " + subLists(list, 7));
 
     }
 
     public static long subLists(List<Long> inputList, long m){
         List<List<Long>> partitions = new ArrayList<>();
-        List<Long> sumList = new ArrayList<>();
-        // create sub lists
         for (int i = 0; i < inputList.size(); i++) {
-            List<Long> list = new ArrayList<>(inputList);
             for (int j = i+1; j <=inputList.size(); j++) {
-                partitions.add(list.subList(i,j));
+                partitions.add(inputList.subList(i,j));
             }
         }
-        // osszeg es modulo képzese
+
+        long max = 0;
         for (int i = 0; i < partitions.size(); i++) {
-            //long sum = partitions.get(i).stream().mapToLong(Long::longValue).sum();
-            long temp = (partitions.get(i).stream().mapToLong(Long::longValue).sum()) % m;
-            sumList.add(temp);
-            //System.out.println("sum "+sum);
-            //
-            //System.out.println("sum%m: "+sum%m);
+            if((sum(partitions.get(i)) % m) > max){
+                max =(sum(partitions.get(i))) % m;
+            }
         }
-        // max keresese:
-        long max = Collections.max(sumList);
-        System.out.println("partitions: " + partitions);
-        System.out.println("max: " + max);
-        System.out.println("inputList : " + inputList);
+
         return max;
+    }
+
+    public static long sum(List<Long> integerList){
+        long sumValue = 0;
+        for (int i = 0; i < integerList.size(); i++) {
+            sumValue += integerList.get(i);
+        }
+        return sumValue;
     }
 }
